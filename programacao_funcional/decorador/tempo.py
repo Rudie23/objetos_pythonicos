b11c4a65dbf944be42bee6e39d44fe6bd3c55e3c
Rudie23 <ruan.diego.anjos@outlook.com>
2023-01-06 16:47:49 -0300

    tkinter

--- programacao_funcional/decorador/tempo.py
+++ programacao_funcional/decorador/tempo.py
@@ -2,13 +2,19 @@ from functools import wraps
 from time import sleep, strftime
 
 
-def logar(f):
-    @wraps(f)  # Para não ser alterada a função passada como parametro, o wraps indicará a função original
-    def executar_com_tempo(*args, **kwargs):
-        print(strftime('%H:%M:%S'))
-        return f(*args, **kwargs)
+def logar(fn=None, *, fmt='%H:%M:%S'):  # O * indica que o parametro fmt deve ser passado de forma nomeada
+    if fn is not None:  # Se você passou o parametro fmt, fn is None
+        return logar(fmt=fmt)(fn)
 
-    return executar_com_tempo
+    def decorator(f):
+        @wraps(f)  # Para não ser alterada a função passada como parametro, o wraps indicará a função original
+        def executar_com_tempo(*args, **kwargs):
+            print(strftime(fmt))
+            return f(*args, **kwargs)
+
+        return executar_com_tempo
+
+    return decorator
 
 
 @logar  # O nome da função será modificado pela função dentro do decorator
@@ -16,7 +22,7 @@ def mochileiro():
     return 42
 
 
-@logar
+@logar(fmt='%d/%m/%Y %H:%M:%S')
 def ola(nome):
     return f'Olá {nome}'
 
