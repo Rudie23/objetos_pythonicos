ad8b6c6096568f3b98e255fcce463f662787acf7
Rudie23 <ruan.diego.anjos@outlook.com>
2023-01-06 16:47:49 -0300

    tkinter

---
+++ Aulas_antigas/tkinter.py
@@ -0,0 +1,18 @@
+
+import tkinter
+from time import strftime
+
+relogio = tkinter.Label()
+relogio.pack()
+relogio['font'] = 'Helvetica 60 bold'
+relogio['text'] = strftime('%H:%M:%S')
+
+
+def tictac():
+    agora = strftime('%H:%M:%S')
+    relogio['text'] = agora
+    relogio.after(100, tictac)
+
+
+tictac()
+relogio.mainloop()
---
+++ programacao_funcional/decorador/decorator.py
@@ -0,0 +1,32 @@
+from inspect import getfullargspec
+from time import sleep, strftime
+
+from decorator import decorator
+
+
+@decorator
+def logar(f, fmt='%H:%M:%S', *args, **kw):
+    print(strftime(fmt))
+    return f(*args, **kw)
+
+
+@logar()
+def mochileiro():
+    return 42
+
+
+@logar(fmt='%d/%m/%Y %H:%M:%S')
+def ola(nome):
+    return f'Olá {nome}'
+
+
+if __name__ == '__main__':
+    print(getfullargspec(ola))
+    print(getfullargspec(mochileiro))
+    print(mochileiro())
+    print(mochileiro.__name__)
+    print(ola('Renzo'))
+    print(ola.__name__)
+    sleep(1)
+    print(mochileiro())
+    print(ola('Luciano'))
