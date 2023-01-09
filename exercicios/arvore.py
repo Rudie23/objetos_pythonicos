41ed1fe6934fc5f17db6c6d2f891ac4ac3a43f59
Rudie23 <ruan.diego.anjos@outlook.com>
2023-01-09 18:07:23 -0300

    Criada a classe de criar um baralho e cartas.

--- exercicios/arvore.py
+++ exercicios/arvore.py
@@ -1,18 +1,29 @@
 
 class Arvore:
     """
-        >>> for noh in Arvore(0, Arvore(-2, Arvore(-4), Arvore(-3)), Arvore(10)):
+        >>> for noh in Arvore(0, Arvore(-2, Arvore(-4), Arvore(-1)), Arvore(10)):
         ...     print(noh)
         -4
-        -1
         -2
+        -1
         0
         10
     """
 
     def __init__(self, valor, esquerda=None, direita=None):
+        self.valor = valor
         self.esquerda = esquerda
         self.direita = direita
+
+    def __iter__(self):
+        if self.esquerda is not None:
+            yield from self.esquerda
+
+        yield self.valor
+        if self.direita is not None:
+            for valor in self.direita:
+                yield valor
+
 
 
 
---
+++ iteracao/baralho.py
@@ -0,0 +1,23 @@
+from iteracao.carta import Carta
+
+
+class Baralho:
+    valores = [str(n) for n in range(2, 11)] + list('JQKA')
+    naipes = 'paus ouros copas espadas'.split()
+
+    def __init__(self):
+        self.cartas = [Carta(v, n) for n in self.naipes for v in self.valores]
+
+    def __len__(self):
+        return len(self.cartas)
+
+    def __getitem__(self, pos):
+        return self.cartas[pos]
+
+    def __setitem__(self, pos, carta):
+        self.cartas[pos] = carta
+
+
+if __name__ == '__main__':
+    for carta in Baralho():
+        print(carta)
