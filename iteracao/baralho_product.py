9fddc18aa1b9518b269534c84dc4c56c306f408f
Rudie23 <ruan.diego.anjos@outlook.com>
2023-01-10 10:09:15 -0300

    Classe baralho criada pela biblioteca itertools e a função product

---
+++ iteracao/baralho_product.py
@@ -0,0 +1,27 @@
+from itertools import product
+from collections import namedtuple
+
+# Cria uma classe e os atributos
+Carta = namedtuple('Carta', 'valor naipe')
+
+
+class Baralho:
+    valores = [str(n) for n in range(2, 11)] + list('JQKA')
+    naipes = 'paus ouros copas espadas'.split()
+
+    def __init__(self):
+        self.cartas = [Carta(v, n) for n, v in product(self.naipes, self.valores)]
+
+    def __len__(self):
+        return len(self.cartas)
+
+    def __getitem__(self, pos):
+        return self.cartas[pos]
+
+    def __setitem__(self, pos, carta):
+        self.cartas[pos] = carta
+
+
+if __name__ == '__main__':
+    for carta in Baralho():
+        print(carta)
